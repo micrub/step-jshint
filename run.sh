65d4c3b3f295ebf3e57057bf3a1b1811816827be
Michael Rubanov <mrubanov@gmail.com>
2016-02-25 04:37:01 +0200

    Fixing depricated cli option.
    --jslint-reporter  Use a jslint compatible reporter (DEPRECATED, use
                             --reporter=jslint instead)

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ ! -n "$WERCKER_JSHINT_VERSION" ]; then
 fi
 
 sudo npm install jshint@$WERCKER_JSHINT_VERSION -g
-jshint . --jslint-reporter > $WERCKER_REPORT_ARTIFACTS_DIR/jshint.xml || true
+jshint . --reporter=jslint > $WERCKER_REPORT_ARTIFACTS_DIR/jshint.xml || true
