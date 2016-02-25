e13c80d4f9368b51ac9f64f2671c9303f3766f96
Michael Rubanov <mrubanov@gmail.com>
2016-02-25 04:38:36 +0200

    Adding non errors to reporting.
    --show-non-errors  Show additional data generated by jshint

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ ! -n "$WERCKER_JSHINT_VERSION" ]; then
 fi
 
 sudo npm install jshint@$WERCKER_JSHINT_VERSION -g
-jshint . --reporter=jslint > $WERCKER_REPORT_ARTIFACTS_DIR/jshint.xml || true
+jshint . --show-non-errors --reporter=jslint > $WERCKER_REPORT_ARTIFACTS_DIR/jshint.xml || true
